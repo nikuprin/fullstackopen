1070f43f91171392da26d46dd85272e49448e068
Nikita Kuprin <nkuprin97@gmail.com>
2022-11-27 09:07:30 +0100

    Add p5.1.

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,96 @@
+import { useState, useEffect, useCallback } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
+import Notification from './components/Notification';
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log('logging in with', username, password);
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      blogService.setToken(user.token);
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user));
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage('wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const fetchBlogs = useCallback(async () => {
+    const blogs = await blogService.getAll();
+    setBlogs(blogs);
+  }, []);
+
+  useEffect(() => {
+    fetchBlogs();
+  }, [fetchBlogs]);
+
+  const loginForm = () => (
+    <div>
+      <h2>login into application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+
+  const blogForm = () => (
+    <div>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+
+  return (
+    <div>
+      <Notification message={errorMessage} />
+      {user === null ? (
+        loginForm()
+      ) : (
+        <div>
+          <h2>blogs</h2>
+          <p>{user.name} logged in</p>
+          {blogForm()}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const baseUrl = '/api/blogs';
+
+let token = null;
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
+
+const getAll = async () => {
+  const request = axios.get(baseUrl);
+  const response = await request;
+  return response.data;
+};
+
+export default { getAll, setToken };
